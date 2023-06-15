76d16e0e04b65009453251cdefad341d0f87899d
gabrielwondracek <gabihw.2005@gmail.com>
2023-06-14 21:23:03 -0300

    python

---
+++ python.py
@@ -0,0 +1,16 @@
+from time import sleep
+
+x = input("Valor de X: ")
+y = input("Valor de Y: ")
+
+print(f'X é {x} e Y é {y}')
+
+delay = 1
+sleep(delay)
+
+if x > y:
+    print("viado")
+else:
+    print("giovani")
+
+
